688553b68ea74806f737e4623f6d8f210a9b1d7b
Jos <jalabrada@gmail.com>
2018-05-08 11:37:45 +0200

    Sentencias de control

---
+++ Control/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //If - else
+            Console.WriteLine("Pulsa la tecla 5");
+            var a = Console.ReadKey();
+            var teclaPulsada = a.KeyChar;
+            
+            if(teclaPulsada == '5')
+            {
+                Console.WriteLine("Has pulsado 5");
+            }
+            else
+            {
+                Console.WriteLine("No has pulsado el 5...");
+            }
+            // Switch
+
+            switch(teclaPulsada)
+            {
+                case 'a':
+                    Console.WriteLine("Añadir elemento");
+                    break;
+                case 'b':
+                    Console.WriteLine("Listar elementos");
+                    break;
+                case 'c':
+                    Console.WriteLine("Eliminar elemento");
+                    break;
+            }
+            
+            //Iteraciones
+
+            // While
+            int i =5;
+            while(i<5)
+            {
+                Console.WriteLine(i++);
+            }
+
+            // Do While
+            int x = 0;
+            do
+            {
+                Console.WriteLine(x++);
+            }
+            while(x<7);
+
+            // For
+            for(int b = 0; b<10; b++)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
