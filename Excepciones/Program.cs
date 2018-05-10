8bff1d2a349d126996b2c115f727820ed9e8e24a
Jos <jalabrada@gmail.com>
2018-05-10 09:29:07 +0200

    API

---
+++ Excepciones/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        try{
+            Program myProgram = null;
+            Console.WriteLine(myProgram.ToString());
+        }
+        catch (System.NullReferenceException )
+        {
+
+        }
+        Console.WriteLine("La vida sigue");
+    }
+}
+
+    public class Vehiculo
+    {
+        public Motor Motor{get; set;}
+    }
+
+    public class Motor
+    {
+        public string Combustible{get; set;}
+        public Motor(string combustible)
+        {
+            Combustible = combustible;
+        }
+    }
+
+}
