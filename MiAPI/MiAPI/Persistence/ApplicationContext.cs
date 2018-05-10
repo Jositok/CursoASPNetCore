7bf08923d86f1cfec275cb3ebc4f78e03c33e03e
Jos <jalabrada@gmail.com>
2018-05-10 10:22:37 +0200

    Compilación correcta

---
+++ MiAPI/MiAPI/Persistence/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using MiAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiAPI.Persistence
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<TodoList> TodoList { get; set; }
+        public DbSet<TodoItem> TodoItem { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+    
+}
+}
